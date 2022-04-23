351bc5523b5c25bb800ac178b725bf820803ad62
Andre Specht <andrespecht@outlook.com>
2022-04-22 19:33:36 -0500

    Exports removed

--- utility.js
+++ utility.js
@@ -1,41 +1,41 @@
 // Add event listener
-export function onEvent(selector, event, callback) {
+function onEvent(selector, event, callback) {
   return selector.addEventListener(event, callback);
 }
 
 // Get HTML element by id
-export function getElement(selector, parent = document) {
+function getElement(selector, parent = document) {
   return parent.getElementById(selector);
 }
 
 // Select HTML element
-export function select(selector, parent = document) {
+function select(selector, parent = document) {
   return parent.querySelector(selector);
 }
 
 // Get a (node) list of HTML elements
-export function selectAll(selector, parent = document) {
+function selectAll(selector, parent = document) {
   return [...parent.querySelectorAll(selector)];
 }
 
 // Print
-export function print(arg) {
+function print(arg) {
   console.log(arg);
 }
 
 // Sleep
-export function sleep(duration) {
+function sleep(duration) {
   return new Promise(resolve => {
     setTimeout(resolve, duration)
   });
 }
 
 // Generate random number between - and including - 'min' and 'max'
-export function randomNumber(min, max) {
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Filter array
-export function filterArray(array, callback) {
+function filterArray(array, callback) {
   return array.filter(callback);
 }

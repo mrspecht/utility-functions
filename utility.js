318309332f670f89b40209d130aa34328cc70f75
Andre Specht <andrespecht@outlook.com>
2024-02-19 12:20:05 -0600

    Update README

--- utility.js
+++ utility.js
@@ -1,58 +1,41 @@
-## JavaScript utils
-
-List of JavaScript utility functions to improve your work efficiency.
-
-
-Using the template
-
-```bash
-
-$ npm i js-func
-
-```
-
-Functions available
-
-```javascript
 // Get HTML element by id
-function getElement(selector) {
+export function getElement(selector) {
   return document.getElementById(selector);
 }
 
 // Select HTML element
-function select(selector) {
+export function select(selector) {
   return document.querySelector(selector);
 }
 
 // Get a list of HTML elements as an array
-function selectAll(selector) {
+export function selectAll(selector) {
   return [...document.querySelectorAll(selector)];
 }
 
 // Add event listener
-function listen(event, selector, callback) {
+export function listen(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
 
 // Sleep
-function sleep(duration) {
+export function sleep(duration) {
   return new Promise(resolve => {
     setTimeout(resolve, duration)
   });
 }
 
 // Generate random number between - and including - 'min' and 'max'
-function randomNumber(min, max) {
+export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Filter array
-function filterArray(array, callback) {
+export function filterArray(array, callback) {
   return array.filter(callback);
 }
 
 // Create an HTML element
-function create(element) {
+export function create(element) {
   return document.createElement(element);
 }
-```
